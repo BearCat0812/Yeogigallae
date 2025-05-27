a7f17fdcbef69561882c0e08c91dd63bf7398e5a
Seung1Jeon <jsw02078@daum.net>
2025-05-27 10:38:20 +0900

    0527

--- Front/src/components/Header.jsx
+++ Front/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './Header.css';
 
 const Header = () => {
   return (
-    <div>
-      <header>
-        <img src="/images/logo.png" alt="logo" />
-      </header>
+    <div class="header">
+        <img src="/images/logo.png" alt="logo" class="logo"/>
+        <button class="search-btn"><i class="fa-solid fa-magnifying-glass"></i></button>
+        <button class="login-btn">로그인/회원가입</button>
     </div>
   )
 }
--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './assets/fontawesome/all.css';
 import Header from './components/Header.jsx'
 
 createRoot(document.getElementById('root')).render(
