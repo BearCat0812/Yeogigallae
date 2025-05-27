70c8aaddb5285e9f5740518ae530320306275f49
BearCat0812 <63222120+BearCat0812@users.noreply.github.com>
2025-05-27 15:59:55 +0900

    25.05.27
    by BC

--- Front/src/components/Header.jsx
+++ Front/src/components/Header.jsx
@@ -1,48 +1,9 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import { Link } from 'react-router-dom';
-import React from 'react'
-=======
 import React from 'react';
->>>>>>> Stashed changes
-=======
-import React from 'react';
->>>>>>> Stashed changes
-=======
-import React from 'react';
->>>>>>> Stashed changes
 import './Header.css';
 import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const navigate = useNavigate();
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
-  return (
-<<<<<<< Updated upstream
-    <div className="container">
-      <div className="header-container">
-        <Link to="/"><img src="/images/logoS.png" alt="logo" className="logo"/></Link>
-        <button className="search-btn"><i className="fa-solid fa-magnifying-glass"></i></button>
-        <button className="login-btn">로그인/회원가입</button>
-      </div>
-=======
-    <div class="header">
-      <img src="/images/logo.png" alt="logo" class="logo" onClick={() => navigate('/')} style={{ cursor: 'pointer' }}/>
-      <button class="search-btn"><i class="fa-solid fa-magnifying-glass"></i></button>
-      <button class="login-btn" onClick={() => navigate('/login')}>로그인</button>
-      <button class="regist-btn" onClick={() => navigate('/regist')}>회원가입</button>
-      <button class="mypage-btn" onClick={() => navigate('/mypage')}>마이페이지</button>
->>>>>>> Stashed changes
-    </div>
-  );
-};
-
-=======
-=======
->>>>>>> Stashed changes
 
   return (
     <div class="header">
@@ -55,8 +16,4 @@ const Header = () => {
   );
 };
 
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 export default Header;
