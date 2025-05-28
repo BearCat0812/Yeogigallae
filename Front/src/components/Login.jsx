392dadaee117fde1208ec363224bd6bdb9987cbe
BearCat0812 <63222120+BearCat0812@users.noreply.github.com>
2025-05-28 10:20:38 +0900

    25.05.28
    by BC

--- Front/src/components/Login.jsx
+++ Front/src/components/Login.jsx
@@ -1,22 +1,46 @@
 import React from 'react';
 import './Login.css';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const Login = () => {
   const navigate = useNavigate();
+
+  const [id, setId] = useState();
+  const [pw, setPw] = useState();
+
+  function login(e) {
+    e.preventDefault();
+    fetch("http://localhost:8080/login", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ id: id, pw: pw })
+    })
+      .then(res => res.json())
+      .then(res => {
+        if (res.success) {
+          sessionStorage.setItem("id", id);
+          alert("Login Response Success");
+        }
+      })
+
+  }
+
   return (
     <div className="login-container">
       <h2>로그인</h2>
       <form className="login-form">
         <div className="form-group">
           <label htmlFor="username">아이디</label>
-          <input type="text" id="username" placeholder="아이디를 입력하세요" />
+          <input type="text" id="username" placeholder="아이디를 입력하세요" onChange={(e) => setId(e.target.value)} />
         </div>
         <div className="form-group">
           <label htmlFor="password">비밀번호</label>
-          <input type="password" id="password" placeholder="비밀번호를 입력하세요" />
+          <input type="password" id="password" placeholder="비밀번호를 입력하세요" onChange={(e) => setPw(e.target.value)} />
         </div>
-        <button type="submit" className="login-button">로그인</button>
+        <button type="submit" className="login-button" onClick={login}>로그인</button>
         <button type="button" className="regist-button" onClick={() => navigate('/regist')}>회원가입</button>
       </form>
     </div>
