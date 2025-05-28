15bc14cdb5970d6931bd5ba79b0089a5f0461209
BearCat0812 <63222120+BearCat0812@users.noreply.github.com>
2025-05-28 09:07:40 +0900

    25.05.28
    by BC

--- Front/src/components/Regist.jsx
+++ Front/src/components/Regist.jsx
@@ -8,15 +8,15 @@ const Regist = () => {
       <form className="regist-form">
         <div className="form-group">
           <label htmlFor="username">이름</label>
-          <input type="text" id="username" placeholder="아이디를 입력하세요" />
+          <input type="text" id="username" placeholder="이름을 입력하세요" />
         </div>
         <div className="form-group">
           <label htmlFor="password">이메일</label>
-          <input type="password" id="password" placeholder="비밀번호를 입력하세요" />
+          <input type="password" id="password" placeholder="이메일을 입력하세요" />
         </div>
         <div className="form-group">
           <label htmlFor="password">아이디</label>
-          <input type="password" id="password" placeholder="비밀번호를 입력하세요" />
+          <input type="password" id="password" placeholder="아이디를 입력하세요" />
         </div>
         <div className="form-group">
           <label htmlFor="password">비밀번호</label>
@@ -24,7 +24,7 @@ const Regist = () => {
         </div>
         <div className="form-group">
           <label htmlFor="password">전화번호</label>
-          <input type="password" id="password" placeholder="비밀번호를 입력하세요" />
+          <input type="password" id="password" placeholder="전화번호를 입력하세요" />
         </div>
         <button type="submit" className="login-button" >회원가입</button>
       </form>
